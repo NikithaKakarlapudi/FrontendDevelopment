7d4a0bad30fa8dafdaa312e51c4735c4fc1d78c8
NikithaKakarlapudi <124078401+NikithaKakarlapudi@users.noreply.github.com>
2023-04-27 22:53:48 +0530

    forms

---
+++ reactjs/src/components/signupform.js
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+import "./SignupForm.css";
+
+function SignupForm() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [phone, setPhone] = useState("");
+  const [address, setAddress] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Form submitted:", username, email, password, phone, address);
+  };
+
+  return (
+    <div className="signup-form-container">
+      <form className="signup-form" onSubmit={handleSubmit}>
+        <h2>Sign up</h2>
+        <div className="form-group">
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="phone">Phone number</label>
+          <input
+            type="tel"
+            id="phone"
+            value={phone}
+            onChange={(e) => setPhone(e.target.value)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="address">Address</label>
+          <input
+            type="text"
+            id="address"
+            value={address}
+            onChange={(e) => setAddress(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit">Sign up</button>
+      </form>
+    </div>
+  );
+}
+
+export default SignupForm;
